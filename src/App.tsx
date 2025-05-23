1f11223f5db51f9ef6028608d28f6401e70a7f7b
Kevin <326.osito@gmail.com>
2025-05-23 19:22:56 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@heroui/button';
+
+function App() {
+  return (
+    <div className='flex flex-wrap gap-4 items-center'>
+      <Button color='default'>Default</Button>
+      <Button color='primary'>Primary</Button>
+      <Button color='secondary'>Secondary</Button>
+      <Button color='success'>Success</Button>
+      <Button color='warning'>Warning</Button>
+      <Button color='danger'>Danger</Button>
+    </div>
+  );
+}
+
+export default App;
